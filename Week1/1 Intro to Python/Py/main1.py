fd6b267418254ac24c68e6850f879f8bb36a2983
Angelica <hernandez.angelica77@gmail.com>
2014-09-01 20:26:15 -0400

    Intro
    Intro to Python

---
+++ Week1/1 Intro to Python/Py/main1.py
@@ -0,0 +1,34 @@
+__author__ = 'NenaH77'
+
+#print "hello world!"
+
+#one lined commnets
+'''
+multiple line commnets
+
+Doc strings
+'''
+
+'''http://legacy.pythong.org/dev/peps/pop-0008/'''
+#no camel case. Use underscores
+first_name = "Kermit"
+last_name = "de Frog"
+#print first_name
+
+#response = raw_input("Enter you name  ")
+#print "Hello there, " , response
+
+
+#expressions
+'''birth_year = 1923
+current_year = 2014
+age = current_year - birth_year
+print age'''
+
+
+birth_year = 1923
+current_year = 2014
+age = current_year - birth_year
+print "You are " + str(age) + " years old" # You can not concatenate strings and Integers. Will need to fix using str()
+# strings to numbers: int(variable)
+
