df1007f9ba723695b9b6111bf08ac6a9fcd61ec2
Angelica <hernandez.angelica77@gmail.com>
2014-09-10 13:00:16 -0400

    forming situation

--- Week2/encapsulated/main.py
+++ Week2/encapsulated/main.py
@@ -10,13 +10,33 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
+#create an instance of my Class Salary
+        van = Salary() #instance of my object
+        van.hr_worked = 40
+        van.hourly = 26.00
+        van.monthly = 0
+        van.annually = 0
+        self.response.write()
+
+
+class Salary(object):
+    def __init__(self):
+       self.hr_worked = 0
+       self.hourly = 0
+       self.monthly = 0
+       self.annually = 0
+       self.__pay_check = 0
+
+    def calc_pay(self): #calculate pay
+        self.__pay_check = ()
 
 
 
 
 
 
-        
+
+
 
 
 
