b3cd1dee98f028990bea36d9cd5017b376349b2c
Angelica <hernandez.angelica77@gmail.com>
2014-09-10 19:08:38 -0400

    monthly earning

--- Week2/encapsulated/main.py
+++ Week2/encapsulated/main.py
@@ -11,24 +11,45 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
 
 #create an instance of my Class Salary
+        #van's salary
         van = Salary() #instance of my object
         van.hr_worked = 40
-        van.hourly = 26.00
-        van.monthly = 0
+        van.hourly = 60.00
+        van.monthly = 9600
         van.annually = 0
-        self.response.write()
+        van.calc_total()
+        self.response.write(van.earn)
+
+        #lily's salary
+        lily = Salary() #instance of my object
+        lily.hr_worked = 40
+        lily.hourly = 26.00
+        lily.monthly = 4160
+        lily.annually = 0
+        lily.calc_total()
+        self.response.write(lily.earn)
 
 
 class Salary(object):
     def __init__(self):
-       self.hr_worked = 0
+       self.hr_worked = 0 #public
        self.hourly = 0
        self.monthly = 0
        self.annually = 0
-       self.__pay_check = 0
+       self.__earn = 0 #private
 
-    def calc_pay(self): #calculate pay
-        self.__pay_check = ()
+    @property #getters
+    def earn(self): #defining my function
+        return self.__earn #getters are always returned
+
+    @earn.setter #setter
+    def earn(self, new_earn):
+        self.__earn = new_earn
+
+
+
+    def calc_total(self): #calculate earnings
+        self.__earn = (((self.hourly * self.hr_worked)*52)/12)
 
 
 
