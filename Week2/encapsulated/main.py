00234e852f3ab986cc4cf000ca787ef5a533798f
Angelica <hernandez.angelica77@gmail.com>
2014-09-13 09:31:07 -0400

    Add info
    Now that I can see my screen lets add my ‘too much’ info back

--- Week2/encapsulated/main.py
+++ Week2/encapsulated/main.py
@@ -47,7 +47,7 @@ class MainHandler(webapp2.RequestHandler):
         self.alexandra.hourly = 16
         self.alexandra.earn_weekly = 576
         self.alexandra.calc_total()
-        print "This is working" + str(self.alexandra.earn_weekly)
+
 
 
         #making array for person's salary link; when link is clicked info will display
@@ -67,21 +67,17 @@ class MainHandler(webapp2.RequestHandler):
             hr_worked = person[salary].hr_worked
             hourly = person[salary].hourly
             earn_weekly = person[salary].earn_weekly
+            net = person[salary].net_pay
 
             #results are displayed once person has been selected
             result = '''
             <div class="content">
             <h2>{name}</h2>
                 <section class="labels">
-                    <p class="info">Hours Worked:</p>
-                    <p class="info">Hourly Pay:</p>
-                    <p class="info">Gross Check:</p>
-                </section>
-
-                <section class="labels">
-                    <p class="info">{hr_worked}</p>
-                    <p class="info">{hourly}</p>
-                    <p class="info">${earn_weekly}</p>
+                    <p class="info">Hours Worked: {hr_worked}</p>
+                    <p class="info">Hourly Pay: {hourly}</p>
+                    <p class="info">Gross Income: ${earn_weekly}</p>
+                    <p class="info">Net Pay: ${net}</p>
                 </section>
             </div>'''
 
@@ -101,7 +97,17 @@ class Salary(object):
         self.name = ""
         self.hr_worked = 0
         self.hourly = 0
+        self.annually = 0
+        self.monthly = 0
         self.__earn_weekly = 0
+        self.__earn_month = 0
+        self.__earn_annual = 0
+        self.__federal_income = 0
+        self.__pension_plan = 0
+        self.__medical_ins = 0
+        self.__deductions = 0
+        self.__net_pay = 0
+
 
 
     #weekly pay
@@ -115,11 +121,73 @@ class Salary(object):
     def earn_weekly(self, new_earn_weekly):
         self.__earn_weekly = new_earn_weekly
 
+    #monthly pay
+    @property #getters
+    def earn_month(self):
+        return self.__earn_month
+
+    @earn_month.setter #setter
+    def earn_month(self, new_earn_month):
+        self.__earn_month = new_earn_month
+
+    #annual pay
+    @property #getters
+    def earn_annual(self):
+        return self.__earn_annual
+
+    @earn_annual.setter #setter
+    def earn_annual(self, new_earn_annual):
+        self.__earn_annual = new_earn_annual
+
+    #federal income
+    @property
+    def federal_income(self):
+        return self.__federal_income
+
+    @federal_income.setter
+    def federal_income(self, new_federal_income):
+        self.__federal_income = new_federal_income
+
+
+    #pension plan
+    @property
+    def pension_plan(self):
+        return self.__pension_plan
+
+    @pension_plan.setter
+    def pension_plan(self, new_pension_plan):
+        self.__pension_plan = new_pension_plan
+
+    #deductions
+    @property
+    def deductions(self):
+        return self.__deductions
+
+    @deductions.setter
+    def deductions(self, new_deductions):
+        self.__deductions = new_deductions
+
+    #net pay
+    @property
+    def net_pay(self):
+        return self.__net_pay
+
+    @net_pay.setter
+    def net_pay(self, new_net_pay):
+        self.__net_pay = new_net_pay
+
+
     #total
     #calculate earnings
     def calc_total(self):
-        total = self.hr_worked * self.hourly
-        self.__earn_weekly = total
+        gross = self.hr_worked * self.hourly
+        self.__earn_weekly = gross
+
+        deduction = self.federal_income - self.pension_plan - 75
+        self.__deductions = deduction
+
+        total = self.earn_weekly - self.deductions
+        self.net_pay = total
 
 
 app = webapp2.WSGIApplication([
