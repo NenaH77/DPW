a51f841491cf49eb36834bdba19e32e8fd7538db
Angelica <hernandez.angelica77@gmail.com>
2014-09-15 14:38:21 -0400

    try & except
    Too much writing…added try and except in another code. Removed the
    notes so I could view it better

---
+++ Week3/inheritance1/main.py
@@ -0,0 +1,73 @@
+'''
+name: Angelica Dinh
+date: sept 15, 2014
+class: dpw
+lab: more inheritance demo (different type of code - try & except)
+
+'''
+import webapp2
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        p = FormPage()
+        p.inputs = [ ['first_name', 'text', 'First Name'], ['last_name', 'text', 'Last Name'], ['Submit', 'submit'] ]
+        self.response.write(p.print_out_form())
+
+class Page(object):
+    def __init__(self):
+        self._head = '''
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body> '''
+
+        self._body = ''
+        self._close = '''
+    </body>
+</html> '''
+
+    def print_out(self):
+        return self._head + self._body + self._close
+
+class FormPage(Page):
+    def __init__(self):
+        super(FormPage, self).__init__()
+        self._form_open = '<form method = "GET">'
+        self._form_close = '</form>'
+        self.__inputs = []
+        self._form_inputs = ''
+        #<input type="text" value="" name="first_name" placeholder="First Name" />
+        #['first_name', 'text', 'First Name']
+        #<input type="text" value="" name="last_name" placeholder="Last Name" />
+        #<input type="submit" value="" name="Submit" />
+
+
+
+    @property
+    def inputs(self):
+        pass
+
+    @inputs.setter
+    def inputs(self, arr):
+        #change my private inputs variable
+        self.__inputs = arr
+        #sort through the mega array and create HTML inputs based on the info there
+        for item in arr:
+            self._form_inputs += '<input type=" ' +item[1]+ ' "name" ' + item[0]
+            #if there is a 3rd item.. add it in....
+            try:
+                self._form_inputs += ' " placeholder=" ' + item[2]+' " />'
+            #otherwise.. end the tag
+            except:
+                self._form_inputs += ' " />'
+
+        print self._form_inputs
+
+    def print_out_form(self):
+        return self._head + self._body + self._form_open + self._form_inputs + self._form_close + self._close
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
