e9d3d5a672f3bbe53cb21fcc2b24c83bc904e545
Angelica <hernandez.angelica77@gmail.com>
2014-09-15 22:13:21 -0400

    update
    images

--- Week3/weather-app/main.py
+++ Week3/weather-app/main.py
@@ -2,7 +2,7 @@
 name: Angelica Dinh
 date: sept 15, 2014
 class: dpw
-lab: polymorphism
+lab: weather-app
 
 '''
 import webapp2
@@ -14,27 +14,40 @@ from xml.dom import minidom
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = FormPage()
-        p.inputs = [['zip', 'text', 'zip code'],['Submit', 'submit']]
+        p.inputs = [['zip', 'text', 'zip code'], ['Submit', 'submit']]
         self.response.write(p.print_out())
 
-        #get info form the API
-        zip = self.request.GET['zip']
-        url = "http://xml.weather.yahoo.com/forecastrss?p=" + zip
+        if self.request.GET: #only if there is a zip variable in the url
+            #get info form the API
+            zip = self.request.GET['zip']
+            url = "http://xml.weather.yahoo.com/forecastrss?p=" + zip
 
-        #assemble the request
-        request = urllib2.Request(url) #we are using the class and accessing the static method in the class. We don't need to create an instance
+            #assemble the request
+            request = urllib2.Request(url) #we are using the class and accessing the static method in the class. We don't need to create an instance
 
-        #use the urllib2 to create and object to get the url
-        opener = urllib2.build_opener()
+            #use the urllib2 to create and object to get the url
+            opener = urllib2.build_opener()
 
-        #use the url to get a result - request info from the API
-        result = opener.open(request)
+            #use the url to get a result - request info from the API
+            result = opener.open(request)
 
-        print result
+            print result
 
-        #parse the XML
-        xmldoc = minidom.parse(result)
-        self.response.write(xmldoc.getElementsByTagName('title')[2].firstChild.nodeValue)
+            #parse the XML
+            xmldoc = minidom.parse(result)
+            self.response.write(xmldoc.getElementsByTagName('title')[2].firstChild.nodeValue)
+            self.content = '<br/>'
+            list = xmldoc.getElementsByTagName('yweather:forecast')
+            #we need to create a loop because we want to add days of the week
+            for item in list:
+                self.content += item.attributes['day'].value
+                self.content += " HIGH: " + item.attributes['high'].value
+                self.content += " LOW: " + item.attributes['low'].value
+                self.content += " CONDITION: " + item.attributes['text'].value
+                self.content += ' <img src="images/'+item.attributes['code'].value+'.png" width ="30"/>'
+                self.content += '<br/>'
+
+            self.response.write(self.content)
 
 class Page(object):
     def __init__(self):
@@ -55,7 +68,6 @@ class Page(object):
     def print_out(self):
         return self._head + self._body + self._close
 
-
 class FormPage(Page):
     def __init__(self):
         #2 ways to write:
@@ -66,7 +78,6 @@ class FormPage(Page):
         self._form_close = '</form>'
         self.__inputs = []
         self._form_inputs = ''
-
 
     @property
     def inputs(self):
