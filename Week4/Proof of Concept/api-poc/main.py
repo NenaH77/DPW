4baf0711656fce6b8d278db1a9cfc8bf11c50d92
Angelica <hernandez.angelica77@gmail.com>
2014-09-17 00:00:30 -0400

    update

--- Week4/Proof of Concept/api-poc/main.py
+++ Week4/Proof of Concept/api-poc/main.py
@@ -14,36 +14,27 @@ from xml.dom import minidom
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        p = Movie()
-        p.options = [['title', 'text', 'Toy Story'], ['name', 'text', 'Tom Hanks' ], ['characters', 'text', 'Woody' ], ['submit', 'submit']]
-        self.response.write(p.options)
+        p = Page()
 
         if self.request.GET:
             #get info from the API
-            mv = Movie()
             title = self.request.GET['title']
             actor = self.request.GET['name']
-            characters = self.request.GET['characters']
-
-            #url from where the information will be requested
-            url = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=3wgzeuyj3ttnqnjbfr5xgafx"
-
-        #3 steps allow us to fetch the url; load the contents and use the contents here in our python code
-            #requests the info from the url
-            request = urllib2.Request(url)
-
-            #special method that allows us to open the results/response and we get to look inside the url
-            opener = urllib2.build_opener()
-
-            #we r fetching the url. in other words, we're telling it to open the url take the results and put them inside the result variable
-            result = opener.open(request)
-
-            print result
 
 
 
+class MovieInfo(object):
+    #adding my 3 steps
+
+    def __init__(self):
+        self.url = "http://api.rottentomatoes.com/api/public/v1.0/" #url from where the information will be requested
+        self.params = ".json?apikey=3wgzeuyj3ttnqnjbfr5xgafx"
+        self.full_url = self.url + self.params
+        self.request = urllib2.Request(self.full_url)#requests the info from the url
+        self.opener = urllib2.build_opener()#special method that allows us to open the results/response and we get to look inside the url
 
 
+        self.result = self.opener.open(self.request)#we r fetching the url. in other words, we're telling it to open the url take the results and put them inside the result variable
 
 
 app = webapp2.WSGIApplication([
