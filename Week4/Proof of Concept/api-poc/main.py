bdf0f9df2b75d6d20fc5d41c7ee2e60a7ea04c53
Angelica <hernandez.angelica77@gmail.com>
2014-09-17 11:56:25 -0400

    ugg!!!!!!

--- Week4/Proof of Concept/api-poc/main.py
+++ Week4/Proof of Concept/api-poc/main.py
@@ -35,12 +35,12 @@ class MainHandler(webapp2.RequestHandler):
 
 class MovieInfo(object):
     def __init__(self):
-        self.url = "http://api.rottentomatoes.com/api/public/v1.0/" #url from where the information will be requested
-        self.params = ".json?apikey=3wgzeuyj3ttnqnjbfr5xgafx"
+        self.url = "http://api.rottentomatoes.com/api/public/v1.0/movies/" #url from where the information will be requested
+        self.params = "?apikey=3wgzeuyj3ttnqnjbfr5xgafx"
 
 
     def send_info(self):
-        full_url = self.url + '/lists/movies/in_theaters' + self.params
+        full_url = self.url + self.params
         request = urllib2.Request(full_url)#requests the info from the url
         opener = urllib2.build_opener()#special method that allows us to open the results/response and we get to look inside the url
         result = opener.open(request)#we r fetching the url. in other words, we're telling it to open the url take the results and put them inside the result variable
