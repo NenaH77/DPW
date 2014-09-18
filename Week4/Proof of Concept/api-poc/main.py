3c164914e54e5e265e2103b43d50cfa791094414
Angelica <hernandez.angelica77@gmail.com>
2014-09-18 14:27:01 -0400

    update

--- Week4/Proof of Concept/api-poc/main.py
+++ Week4/Proof of Concept/api-poc/main.py
@@ -21,14 +21,34 @@ class MainHandler(webapp2.RequestHandler):
         if self.request.GET:
             my_movie = self.request.GET["movie"]#passes url into MovieInfo class
             movinfo = MovieInfo() #creates an instance of my MovieInfo class
+            #sending request to api method
+            movinfo.apiInfo()
 
 
 class MovieInfo(object):
     #MovieInfo will allow me to fetch the url; load the contents and use the contents here in my python code
     def __init__(self):
-        self.movie = ""
+        self.__movie = ""
+        self.jsondoc = ""
 
+    def apiInfo(self):
+        #get info from API
+        url = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=3wgzeuyj3ttnqnjbfr5xgafx&q="+ self.__movie + "&page_limit"
+        #request the info from the url
+        request = urllib2.Request(url)
+        #special method that allows us to open the results and we get to look inside the url
+        opener = urllib2.build_opener()
+        #we r fetching the url. in other words, we're telling it to open the url take the results and put them inside the result variable
+        result = opener.open(request)
 
+        #parsing the JSON
+        jsondoc = json.load(result)
+        #stores my variables
+        self.current_movie_title = jsondoc["movies"][0]["title"]
+        self.current_movie_rating = jsondoc["movies"][0]["mpaa_rating"]
+        self.current_movie_critic_rating = jsondoc["movies"][0]["ratings"]["critics_rating"]
+        self.current_movie_synopsis = jsondoc["movies"][0]["synopsis"]
+        self.current_movie_actor = jsondoc["movies"][0]["abridged_cast"][0]["name"]
 
 
 app = webapp2.WSGIApplication([
