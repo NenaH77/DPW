f4a1b24e00e8e284afef2888e9b332da9fff9e72
Angelica <hernandez.angelica77@gmail.com>
2014-09-18 13:50:00 -0400

    update

--- Week4/Proof of Concept/api-poc/main.py
+++ Week4/Proof of Concept/api-poc/main.py
@@ -21,7 +21,12 @@ class MainHandler(webapp2.RequestHandler):
         if self.request.GET:
             my_movie = self.request.GET["movie"]#passes url into MovieInfo class
             movinfo = MovieInfo() #creates an instance of my MovieInfo class
-            
+
+
+class MovieInfo(object):
+    #MovieInfo will allow me to fetch the url; load the contents and use the contents here in my python code
+    def __init__(self):
+        self.movie = ""
 
 
 
