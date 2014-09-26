078c23e3b046edc519fdb3278af4235ce18a5783
Angelica <hernandez.angelica77@gmail.com>
2014-09-26 15:42:44 -0400

    practice

--- Week4/api-personal-practice/main.py
+++ Week4/api-personal-practice/main.py
@@ -12,7 +12,8 @@ import json
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = FormPage
-        p.inputs = [['books', 'text', 'Book title'], ['author', 'text', 'Author Name'], ['submit', 'Submit']]
+        p.inputs = [['books', 'text', 'Book title'] , ['submit', 'Submit']]
+        #['author', 'text', 'Author Name']
 
 
 
@@ -21,7 +22,7 @@ class MainHandler(webapp2.RequestHandler):
             author = self.request.GET['author']
             bm = BookModel()
             bm.books = self.request.GET['books']
-            bm.author = self.request.GET['author']
+            #bm.author = self.request.GET['author']
             bm.callApi()
 
             print bm.dos
@@ -58,13 +59,51 @@ class BookView(object):
 
 
 
-
-
 class BookModel(object):
-    pass
+    def __init__(self):
+        self.__url = "https://www.googleapis.com/books/v1/volumes?q="
+        self.__books = ""
+        #self.__author = ""
+        self.__jsondoc = ""
+
+    def callApi(self):
+        request = urllib2.Request(self.__url + self.__books)
+        opener = urllib2.build_opener()
+        result = opener.open(request)
+
+        self.__jsondoc = json.load(result)
+
+        books = self.__jsondoc['items']
+
+        self._dos = []
+
+        for item in books:
+            do = BookData()
+            do.title = item['items'][0]['volumeInfo'][0]['title']
+            do.author = item['items'][0]['volumeInfo'][0]['authors']
+            do.description = item['items'][0]['volumeInfo'][0]['description']
+            do.price = item['items'][0]['saleInfo']['listPrice']['amount']
+            self._dos.append(do)
+
+    @property
+    def dos(self):
+        return self._dos
+
+    @property
+    def books(self):
+        pass
+
+    @books.setter
+    def books(self, b):
+        self.__books = b
+
 
 class BookData(object):
-    pass
+    def __init__(self):
+        self.title = ''
+        self.author = ''
+        self.description = ''
+        self.price = ''
 
 
 class Page(object):
@@ -93,8 +132,29 @@ class Page(object):
 
 
 class FormPage(Page):
-    pass
+    def __abs__(self):
+        super(FormPage, self).__init__()
+        self._form_open = '<form method = "GET">'
+        self.__form_close = '</form>'
+        self.__inputs = []
+        self._form_inputs = ''
 
+    @property
+    def inputs(self):
+        pass
+
+    @inputs.setter
+    def inputs(self, arr):
+        self.__inputs = arr
+        for item in arr:
+            self._form_inputs +='<input type"' + item[1]+ '"name="' +item[0]
+            if len(item)>2:
+                self._form_inputs +='"placeholder="' +item[2]+'"/>'
+            else:
+                self._form_inputs += '"/>'
+
+    def print_out(self):
+        return self._head + self._form_open + self._form_inputs + self.__form_close + self._body + self._close
 
 
 app = webapp2.WSGIApplication([
